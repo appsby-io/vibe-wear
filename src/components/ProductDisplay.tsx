c652ea10291abed7b74412702a01684a12bc127e
appsby-io <einkauf@appsby.io>
2025-06-19 18:57:59 +0200

    Updated ProductDisplay.tsx

--- src/components/ProductDisplay.tsx
+++ src/components/ProductDisplay.tsx
@@ -148,7 +148,7 @@ export const ProductDisplay: React.FC<ProductDisplayProps> = ({
               className="relative bg-white rounded-2xl overflow-hidden"
               style={{ 
                 width: 'auto',
-                height: window.innerWidth < 1024 ? '340px' : '560px' 
+                height: window.innerWidth < 1024 ? '360px' : '560px' 
               }}
             >
               {/* Product Base */}
