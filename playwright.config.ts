f0b99f281780e26ba8e0fe52ee9c713dc1b6bb37
appsby-io <einkauf@appsby.io>
2025-06-22 23:03:03 +0200

    Fix Netlify Edge Functions 404 error

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://localhost:5173',
+    baseURL: 'http://localhost:8888',
     trace: 'on-first-retry',
   },
 
@@ -28,8 +28,8 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:5173',
+    command: 'netlify dev',
+    url: 'http://localhost:8888',
     reuseExistingServer: !process.env.CI,
   },
 });
